e1680e494ab3dfe3d982448a5c764fd0952f38f6
EdgarGiovaniRincon <edgar.gio005@gmail.com>
2025-05-12 17:20:02 -0600

    Actualización para que la app funcione en Render

--- app.py
+++ app.py
@@ -150,4 +150,6 @@ def servir_imagen(filename):
 # =========================================
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Obtiene el puerto de Render
+    app.run(debug=True, host='0.0.0.0', port=port)  # Asegura que se escuche en todas las IPs
+
